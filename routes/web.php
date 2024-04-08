3e3949d16092447c19d66ce787f8a7891a830bab
Martin Dub <martin.dub@dek-cz.com>
2024-04-08 06:56:45 +0200

    Prod version of Vue.js, routes edited

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ use App\Http\Controllers;
 |
 */
 
-Route::get('/', [Controllers\HomepageController::class, 'index'])->name('homepage');
-
-Route::post('/sendMail', [Controllers\EmailsController::class, 'sendMail'])->name('sendMail');
-
 Route::get('/login', [Controllers\AuthController::class, 'login'])->name('login');
 Route::post('/authenticate', [Controllers\AuthController::class, 'authenticate'])->name('authenticate');
 
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [Controllers\HomepageController::class, 'index'])->name('homepage');
+
+    Route::post('/sendMail', [Controllers\EmailsController::class, 'sendMail'])->name('sendMail');
+
     Route::any('/admin', [Controllers\AdminController::class, 'dashboard'])->name('admin.dashboard');
     Route::match(['get', 'post'], '/update-announcement', [Controllers\AdminController::class, 'announcement'])->name('set-announcement');
 
