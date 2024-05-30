0c2f535648b99f34cadbc30a0d67ae51da3c5b3d
Martin Dub <martin.dub@dek-cz.com>
2024-05-30 10:04:10 +0200

    Laravel 11

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::get('/', [Controllers\HomepageController::class, 'index'])->name('homepage');
 
 Route::post('sendMail', [Controllers\EmailsController::class, 'sendMail'])->name('sendMail');
